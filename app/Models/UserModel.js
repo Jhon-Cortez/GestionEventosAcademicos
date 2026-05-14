5c9a391b68f42308f42b429406b93de282b597bd
Jhon-Cortez <cortezvargasjhon01@gmail.com>
2026-05-14 13:25:06 -0500

    fix: password dosnt allow empty fields

--- app/Models/UserModel.js
+++ app/Models/UserModel.js
@@ -3,16 +3,17 @@ export class UserModel {
     static validate(email, password) {
 
         if (email !== '' && password !== '') {
-
-            window.location.href =
-                window.location.href ="../Views/dashboard.html";
-
+            if(password.trmim() === ""){
+                alert("Contraseña no puede ser solo espacios en blanco");
+                return false;
+            }
             return true;
         }
-
         alert("Campos vacíos");
 
         return false;
     }
 
 }
+//window.location.href =
+            //window.location.href ="../Views/dashboard.html";
